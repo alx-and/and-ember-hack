1ce283766fba6f93f7dcb31ad376fe3ff873bf48
Alex R <alex.reilly@and.digital>
2022-12-14 11:06:28 +0000

    Initial ember app creation

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'and-ember-hack/app';
+import config from 'and-ember-hack/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
