389a645d1b30a245fd3d5046caba90e47ab15d5f
Alex R <alex.reilly@and.digital>
2022-12-15 19:29:04 +0000

    Add front-card image links

--- app/services/cocktail-data.js
+++ app/services/cocktail-data.js
@@ -2,6 +2,21 @@ import Service from '@ember/service';
 import cocktailApiUtil from '../utils/cocktail-api';
 import pickPropertiesUtil from '../utils/pick-properties';
 
+const imgLinks = [
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131584/and-ember-hack/door-1_kojvjh.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131584/and-ember-hack/door-2_eo5al1.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131584/and-ember-hack/door-3_ir8hok.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131584/and-ember-hack/door-4_mtz6wu.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131584/and-ember-hack/door-5_f6w6vt.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131584/and-ember-hack/door-6_nejpen.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131585/and-ember-hack/door-7_q8tw43.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131585/and-ember-hack/door-8_sfzuuu.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131585/and-ember-hack/door-9_c7xazp.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131585/and-ember-hack/door-10_eldtlz.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131585/and-ember-hack/door-11_mdios2.png',
+  'https://res.cloudinary.com/dwur5dw76/image/upload/v1671131585/and-ember-hack/door-12_r7xbyo.png',
+];
+
 export default class CocktailDataService extends Service {
   alcoholicOptions = null;
   nonAlcoholicOptions = null;
@@ -24,7 +39,7 @@ export default class CocktailDataService extends Service {
     const drinkOptions = await options.reduce(async (acc, item, index) => {
       const drinkInfo = await cocktailApiUtil.getCocktailById(item.idDrink);
       const pickedProperties = pickPropertiesUtil.pickProperties(drinkInfo);
-      pickedProperties.imagePath = `door-${index + 1}.png`;
+      pickedProperties.imagePath = imgLinks[index];
       (await acc).push(pickedProperties);
       return acc;
     }, []);
